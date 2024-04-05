e62111aa53eef1fb5768e0b4f5f2adba879ced6a
Shahriar Tamim <abdnoman1616@gmail.com>
2024-04-05 12:21:47 +0600

    Added: Navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+
+import { useState } from "react";
+import "./navbar.scss"
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false)
+  return (
+    <nav>
+      <div className="left">
+        <a href="" className="logo">
+          <img src="/logo.png" alt="lama estate" />
+          <span>LamaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contacts</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+        <div className="menuIcon" onClick={()=>setOpen(!open)}>
+          <img src="/menu.png" alt="" />
+        </div>
+        <div className={open? "menu active":"menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contacts</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar

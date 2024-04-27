0fd2406cd5cb6722526c7597af49c774bfe4b621
Shahriar Tamim <abdnoman1616@gmail.com>
2024-04-27 23:20:58 +0600

    Update : forgot

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import "./card.scss"
+import { Link } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({item}) => {
   return (
-    <div className='card'> 
-      card
+    <div className="card">
+      <Link to={`/${item.id}`} className="imgContainer">
+        <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bath.png" alt="" />
+              <span>{item.bedroom} Bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="" />
+              <span>{item.bathroom} Bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Card
